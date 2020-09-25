a7bd476535b973d13856cb721fd097d1d7b9a6bf
Jairo <jairo.soares@gmail.com>
2020-09-25 17:13:33 -0300

    Test platform!

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 const { resolve} = require('path')
-const { app, Menu, Tray } = require('electron')
+const { app, Menu, Tray, systemPreferences, nativeTheme } = require('electron');
+const { get } = require('http');
+
+const clippings = [];
+clippings.push('Filhao')
+clippings.push('Bora pra cima!')
+
+const getIcon = () => {
+  return process.platform === 'win32' ? 'iconTemplate@2x.ico' : 'iconTemplate.png';
+};
 
 let tray = null
 app.whenReady().then(() => {
-  tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'))
+  tray = new Tray(resolve(__dirname, 'assets', getIcon()))
+  //Test if Windows
+  if (process.platform === 'win32') {
+    
+    tray.on('click', tray.popUpContextMenu);
+  } else {
+    console.log('platform is ' + process.platform);
+  }
+  console.log('isDarkMode ? ' + nativeTheme.shouldUseDarkColors)
+
+  //Hides the dock icon if running on macOS
+  if (app.dock) app.dock.hide();
+  updateMenu();
+})
+
+const updateMenu = () => {
   const contextMenu = Menu.buildFromTemplate([
     { label: 'View Settings', type: 'radio' },
+    { label: 'Add Clipping', click() {addClipping()}},
+    { type: 'separator'},
+    ...clippings.map((clipping, index) => ({label: clipping})),
+    { type: 'separator'},
+    
     { label: 'This is checked', type: 'radio', checked: true },
-    { label: 'Exit', type: 'radio' }    
+    { label: 'Exit', type: 'radio', click() {app.quit()} }    
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
-})
+}
+
+const addClipping = () => {
+  console.log('kkk')
+}
